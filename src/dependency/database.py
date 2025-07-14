a0603e442b443e70a2ec1ec411f1a42a0f101209
lyapin <lajpinkonstantin@gmail.com>
2025-07-14 21:08:12 +0300

    fix model Base

--- src/dependency/database.py
+++ src/dependency/database.py
@@ -7,9 +7,7 @@ from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from src.config import db_settings
-from src.dependency.models import User
-
-Base: DeclarativeMeta = declarative_base()
+from src.dependency.models import User, Base
 
 
 def make_connection_string() -> str:
