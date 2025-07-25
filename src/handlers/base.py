005c04c67d0061cca2fccc1d2493fdb48e8f7160
lyapin <lajpinkonstantin@gmail.com>
2025-07-26 00:35:03 +0300

    update readme

--- src/handlers/base.py
+++ src/handlers/base.py
@@ -1,14 +1,7 @@
 from fastapi import APIRouter
-from src.dependency.database import create_db_and_tables
 base_router = APIRouter()
 
 
 @base_router.get("/ping")
 async def check():
     return {"ping": "pong!"}
-
-
-"""@base_router.on_event("startup")
-async def on_startup():
-    # Not needed if you setup a migration system like Alembic
-    await create_db_and_tables()"""
